a91914cc10f27e7ea87962a962d2912ba5c6ea0d
MSthe00 <marcoschoeb@gmail.com>
2017-10-02 14:42:03 +0000

    Fixed CSS, Info page almost ready

--- scripts/goopen.sh
+++ scripts/goopen.sh
@@ -1,4 +1,4 @@
-echo 50 > /sys/class/tacho-motor/motor1/speed_sp
+echo -300 > /sys/class/tacho-motor/motor1/speed_sp
 echo run-forever > /sys/class/tacho-motor/motor1/command
 sleep 0.5
 echo stop > /sys/class/tacho-motor/motor1/command
